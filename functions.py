6354dfb5f3f4f6eac9ae416e2f855dd73c217af4
NafunaAnitahSoniah <nafunasoniah@gmail.com>
2025-06-13 10:17:00 +0300

    Friday, 13 June 2025 Python class

---
+++ functions.py
@@ -0,0 +1,20 @@
+#Functions are the backbone of a programming language
+
+#A function is a name block of code (group of statements performing something)
+#Functions logically group your code
+
+num1, num2 = 10,20
+print(num1 + num2)
+
+
+def Add(): #Add() is the name of the function
+    num1, num2 = 10,20
+    print(num1 + num2) #won't print until you call the code to run
+
+Add() #This calls the code to run.
+
+def Subtract(): #name of the function
+    num3, num4 = 40,20
+    print(num3 - num4) #won't print until you call the code to run
+
+Subtract() #This calls the code to run.
--- loops.py
+++ loops.py
@@ -1,6 +1,7 @@
 #We use loops to order a computer to do something/ iterate through a given task until a certain condition is false
 #the false condition gives the computer a stoppage point otherwise the computer memory maybe overwhelmed
 
+#FOR LOOP
 for item in range(10):  #range is a function
     print(item) #computer will print from 0 to 9
 
@@ -20,7 +21,97 @@ for letter in name: #the function 'letter' iterates each individual letter in th
 for character in name: #the function 'character' iterates each individual letter in the string 'Soniah'
     print(character) #the computer will then print one by one character 
 
+numbers = [2, 3, 67, 90, 600, 10]
 
+countries = ["Uganda", "Kenya", "Hungary", "Germany", "Poland", "Sweden"]
+
+cities = ["Kampala", "Cairo", "Nairobi", "Frankfurt", "Paris", "London"]
+
+for country in countries:
+    print(country)
+# "Country" is a swap space; it is a temporary memory
+# sort of a counter where everything is first received before it goes somewhere.
+#the computer accesses the value individually, one by one
+#same appliesin the for loop below, but in this case our items are cities
+for city in cities: 
+    print(city)
+
+#to print Uganda and Kenya from the countries list
+for country in countries:
+    if country == "Uganda":
+        print("************")
+        print(country)
+    elif country == "Kenya":
+        print("************")
+        print(country)
+    else:
+        pass #means ignore the rest; tells the computer to stop printing when the condition is met
+
+#to print Frankfurt and Paris
+for city in cities:
+    if city == "Frankfurt":
+        print("************")
+        print(city)
+    elif city == "Paris":
+        print("*********")
+        print(city)
+        print("***********") #used to demarcate output and make the code readable
+    else:
+        pass
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+
+
+for number in numbers:
+    if number % 2 != 0:
+        print(number)
+
+#Using a for loop and if statement, print out even numbers between 1 and 100
+for number in range(1,100): #prints even numbers; where it finds an odd number, it prints the else condition
+    if number % 2 == 0:
+        print("*******")
+        print(number)
+    else:
+        print("All numbers are odd")
+
+for number in range(1,100): #prints even numbers only
+    if number % 2 == 0:
+        print("*******")
+        print(number)
+  
+
+for number in range(1,100,2): #to print odd numbers only
+    if number % 2 != 0:
+        print("*******")
+        print(number)
+  
+for number in range(1,100): #prints odd numbers, where it finds an even number, it prints the else condition
+    if number % 2 != 0:
+        print("*******")
+        print(number)
+    else:
+        print("All numbers are even")
+
+for number in range(1,100,2): #to print odd numbers only
+    if number % 2 != 0:
+        print("*******")
+        print(number)
+    else:
+        pass
+
+
+#WHILE LOOP
+#one of the least used loops because as it iterates into the values, it checks the conditions atleast once
+#usaually evaluates to "True" so it won't close
+var1 = 1
+while var1 <= 3:
+    print("=======")
+    print(var1)
+    var1 += 1
+
+#try to avoid loops if possible 
 
 
 
